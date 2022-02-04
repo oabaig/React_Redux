7cb21d5fcfeae5db95946667a61e47fcb696841c
Omar Baig <omarbaig1998@gmail.com>
2022-02-04 11:45:34 -0600

    added semantic ui styling to searchbar

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,8 +3,8 @@ import SearchBar from './SearchBar';
 
 export default function App() {
 	return (
-		<>
+		<div classNameName="ui container" style={{ marginTop: '10px' }}>
 			<SearchBar />
-		</>
+		</div>
 	);
 }
