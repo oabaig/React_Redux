97dac40dcd945d6fbde5b26237aa5f64198a61f0
Omar Baig <OmarBaig1998@gmail.com>
2022-03-30 08:58:47 -0500

    finished accordion

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-	const renderedItems = items.map((item) => {
+	const [activeIndex, setActiveIndex] = useState(null);
+
+	const onTitleClick = (index) => {
+		setActiveIndex(index);
+	};
+
+	const renderedItems = items.map((item, index) => {
+		let titleClassName = 'title';
+		let contentClassName = 'content';
+
+		if (activeIndex === index) {
+			titleClassName += ' active';
+			contentClassName += ' active';
+		}
+
 		return (
-			<div key={item.title}>
-				<div className="title active">
+			<React.Fragment key={item.title}>
+				<div
+					className={titleClassName}
+					onClick={() => {
+						onTitleClick(index);
+					}}
+				>
 					<i className="dropdown icon"></i>
 					{item.title}
 				</div>
-				<div className="content active">
+				<div className={contentClassName}>
 					<p>{item.content}</p>
 				</div>
-			</div>
+			</React.Fragment>
 		);
 	});
 
