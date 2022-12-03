dc76749f2ee49e6fe77c3030e0051dfb808c4798
Omar Baig <OmarBaig1998@gmail.com>
2022-12-02 19:01:17 -0600

    save

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
+
+class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client
+                .init({
+                    clientId:
+                        '619623667858-62dd8r2766a6viqi19q6ul26q6qok650.apps.googleusercontent.com',
+                    scope: 'email',
+                    plugin_name: 'streamy',
+                })
+                .then(() => {
+                    this.auth = window.gapi.auth2.getAuthInstance();
+                    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                    this.auth.isSignedIn.listen(this.onAuthChange);
+                });
+        });
+    }
+
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
+    };
+
+    OnSignInClick = () => {
+        this.auth.signIn();
+    };
+
+    OnSignOutClick = () => {
+        this.auth.signOut();
+    };
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null;
+        } else if (this.state.isSignedIn) {
+            return (
+                <button
+                    onClick={this.OnSignOutClick}
+                    className="ui red google button"
+                >
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
+        } else {
+            return (
+                <button
+                    onClick={this.OnSignInClick}
+                    className="ui red google button"
+                >
+                    <i className="google icon" />
+                    Sign In with Google
+                </button>
+            );
+        }
+    }
+
+    render() {
+        return this.renderAuthButton();
+    }
+}
+
+export default connect(null, { signIn, signOut })(GoogleAuth);
---
+++ streams/client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const store = configureStore({ reducer: reducers });
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+);
